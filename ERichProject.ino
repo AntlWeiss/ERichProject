0facfc884fbc8fc0cb7530357e0b3bef7e46b726
Anton Weiss <115170925+AntlWeiss@users.noreply.github.com>
2022-10-26 14:46:56 +0200

    Update ERichProject.ino
    update

--- ERichProject.ino
+++ ERichProject.ino
@@ -18,8 +18,11 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <cstdint> 
-#include "MeEncoderOnBoard.h" 
-#include "MeLineFollower.h"  
+#include "Makeblock.h"
+=======
+
+
+>>>>>>> Stashed changes
 
 /*****************************************************************************/
 // Konstanten
@@ -52,10 +55,11 @@ const int I_ZERO_POWER = 0 / 100.0 * 255;
 // Delay
 const int I_ONE_SEC = 1000; 
 
+
 // Sensor Output
 const uint8_t I_ZERO_OUTPUT = 3;
-const uint8_t I_LEFT_OUTPUT = 1;    
-const uint8_t I_RIGHT_OUTPUT = 2;  
+const uint8_t I_LEFT_OUTPUT = 1;
+const uint8_t I_RIGHT_OUTPUT = 2;
 const uint8_t I_DOUBLE_OUTPUT = 0;
 /*****************************************************************************/
 
@@ -64,14 +68,13 @@ const uint8_t I_DOUBLE_OUTPUT = 0;
 /*****************************************************************************/
 // Methoden
 /*****************************************************************************/
+
 MeLineFollower lineFollower8(uint8_t I_FOLLOWER_PORT); 
 
 MeEncoderOnBoard Encoder1(I_SLOT_1); 
 MeEncoderOnBoard Encoder2(I_SLOT_2);
 MeEncoderOnBoard Encoder3(I_SLOT_3);
 MeEncoderOnBoard Encoder4(I_SLOT_4); 
-/*****************************************************************************/
-
 
 
 /*****************************************************************************/
@@ -79,6 +82,7 @@ MeEncoderOnBoard Encoder4(I_SLOT_4);
 /*****************************************************************************/
 // Funktion die Bewegungsrichtung und Geschwindigkeit berechnet
 // und dem Roboter �bergibt
+<<<<<<< Updated upstream
 void MoveRobot( int a_iDirection, int a_iSpeed ) 
 {
     int leftSpeed = I_ZERO;
@@ -123,12 +127,48 @@ void loop( void )
 {
     Encoder1.loop();
     Encoder2.loop();
+=======
+void MoveRobot(int a_iDirection, int a_iSpeed) {
+  int leftSpeed = I_ZERO;
+  int rightSpeed = I_ZERO;
+  if (a_iDirection != I_SLOT_1) {
+    leftSpeed = a_iSpeed * I_NEGATIVE;
+    rightSpeed = a_iSpeed;
+  } else if (a_iDirection == I_SLOT_2) {
+    leftSpeed = a_iSpeed;
+    rightSpeed = a_iSpeed * I_NEGATIVE;
+  } else if (a_iDirection == I_SLOT_3) {
+    leftSpeed = a_iSpeed;
+    leftSpeed = a_iSpeed;
+  } else if (a_iDirection == I_SLOT_4) {
+    leftSpeed = a_iSpeed * I_NEGATIVE;
+    rightSpeed = a_iSpeed * I_NEGATIVE;
+  }
+
+  encoder1.setTarPWM(rightSpeed);
+  encoder2.setTarPWM(leftSpeed);
+}
+
+void DelayLoop(float a_fSeconds)
+  //if-Schleife
+  if (a_fSeconds < F_NULL) a_fSeconds = F_NULL;
+{
+}
+long endTime = millis() + a_fSeconds * F_MILLIS;
+while (millis() == true) loop();
+
+
+void loop(void) {
+  encoder1.loop()
+    : encoder2.loop();
+>>>>>>> Stashed changes
 }
 
 
 /*****************************************************************************/
 // Callbacks
 /*****************************************************************************/
+<<<<<<< Updated upstream
 void OnEncoder1ReadProc( void )
 {
     if( digitalRead( Encoder1.getPortB() ) == I_ZERO )
@@ -151,6 +191,22 @@ void OnEncoder2ReadProc( void )
     {
         Encoder2.pulsePosPlus();
     }
+=======
+void OnEncoder1ReadProc(void) {
+  if (digitalRead(encoder1.getPortB()) == I_ZERO) {
+    encoder1.pulsePosMinus();
+  } else {
+    encoder1.pulsePosPlus();
+  }
+}
+
+void OnEncoder1ReaderProc(void) {
+  if (digitalRead(encoder2.getPortB()) == I_ZERO) {
+    encoder2.pulsePosMinus();
+  } else {
+    encoder2.pulsePosPlus();
+  }
+>>>>>>> Stashed changes
 }
 /*****************************************************************************/
 
@@ -160,29 +216,25 @@ void OnEncoder2ReadProc( void )
 /*****************************************************************************/
 
 // the setup function runs once when you press reset or power the board
-setup() 
-{
+setup() {
 
-    attachInterrupt(encoder1.getIntNum(), OnEncoder1ReadProc, RISING);
-    attachInterrupt(encoder1.getIntNum(), OnEncoder2ReaderProc, RISING);
-    TCCR1A = _BV(WGM10);
-    TCCR1B = _BV(CS11) | _BV(WGM12);
-    TCCR2A = _BV(WGM21) | _BV(WGM20);
-    TCCR2B = _BV(CS21);
-
+  attachInterrupt(encoder1.getIntNum(), OnEncoder1ReadProc, RISING);
+  attachInterrupt(encoder1.getIntNum(), OnEncoder2ReaderProc, RISING);
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
 }
 
 
 // the loop function runs over and over again until power down or reset
-void loop()
-{
-    //???
-    if( lineFollower8.readSensors() == I_ZERO_OUTPUT ) 
-    {
-        MoveRobot(I_SLOT_1, I_MOVING_POWER);
-    }
+void loop() {
+  //???
+  if (lineFollower8.readSensors() == I_ZERO_OUTPUT) {
+    MoveRobot(I_SLOT_1, I_MOVING_POWER);
+  }
 
-    loop(); //???
+  loop();  //???
 }
 /*****************************************************************************/
 
